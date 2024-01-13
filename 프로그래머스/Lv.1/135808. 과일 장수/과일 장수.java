b052c85754174e0659ab54d5bfb66417aebd03f3
HongBeom <79892287+son7877@users.noreply.github.com>
2024-01-13 18:20:48 +0900

    [level Lv.1] Title: 과일 장수, Time: 0.34 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Arrays.sort(score);
+        for(int i = score.length; i>=m; i-=m){
+            answer += score[i-m]*m;
+        }
+        return answer;
+    }
+}
