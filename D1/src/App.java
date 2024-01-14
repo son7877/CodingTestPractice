f2d0cefae1fa2077f7a27ab6343c68e33f8c922f
HongBeom <79892287+son7877@users.noreply.github.com>
2024-01-14 21:42:37 +0900

    Add files via upload

---
+++ D1/src/App.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        for(int i = N;i>=0;i--){
+            System.out.print(i+" ");
+        }
+    }
+}
