2b6da59eda5018759c20aab0bb4924e68e7880b1
HongBeom <79892287+son7877@users.noreply.github.com>
2024-03-10 20:07:55 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 268 ms, Memory: 15292 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i=1;i<=num;i++){
+            for(int j=0;j<num-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
