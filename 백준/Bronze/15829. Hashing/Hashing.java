62526d903bdef0f040d26e4e1a6724173947168d
HongBeom <79892287+son7877@users.noreply.github.com>
2024-03-30 17:24:44 +0900

    [Bronze II] Title: Hashing, Time: 208 ms, Memory: 17664 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
-
 public class Main {
+    static final int M = 1234567891;
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int num = Integer.parseInt(sc.nextLine());
         String str = sc.nextLine();
         char []  c =str.toCharArray();
         int [] intArr = new int[num];
 
-        int sum =0;
+        long sum =0;
+        long r =1;
         for(int i=0;i<intArr.length;i++){
             intArr[i]= c[i]-'a'+1;
-            sum += intArr[i]*Math.pow(31,i);
+            sum += intArr[i]*r%M;
+            r = (r*31)%M;
         }
-        System.out.println(sum%1234567891);
+        System.out.println(sum%M);
 
     }
 }
