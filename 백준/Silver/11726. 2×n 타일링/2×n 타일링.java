29dbfd4b1d7ef8ecca053210d95f8c4d19e7dc9a
HongBeom <79892287+son7877@users.noreply.github.com>
2024-04-30 16:58:42 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long [] pibo = new long[n+1];
+
+        if(n==0) pibo[0]=1;
+        else{
+            pibo[0]=1;
+            pibo[1]=2;
+            for(int i=2;i<n;i++){
+                pibo[i]=(pibo[i-2]+pibo[i-1])%10007;
+            }
+        }
+        System.out.println(pibo[n-1]);
+    }
+}
