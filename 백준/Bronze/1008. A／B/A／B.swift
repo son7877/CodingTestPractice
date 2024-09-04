73b91ed19b76af31972d89e74b50eba9568a9f16
HongBeom <79892287+son7877@users.noreply.github.com>
2024-09-04 17:51:49 +0900

    [Bronze V] Title: A/B, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  BJ1008
+//
+//  Created by 안홍범 on 9/4/24.
+//
+
+import Foundation
+
+let input = readLine()!.split(separator:" ").map{Double($0)!}
+
+print(input[0]/input[1])
+
+
