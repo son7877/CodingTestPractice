47a2437293105076236313c73ff98e2a7cc7ea86
HongBeom <79892287+son7877@users.noreply.github.com>
2024-09-04 17:32:37 +0900

    [Bronze IV] Title: 숫자의 합, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  BJ11720
+//
+//  Created by 안홍범 on 9/4/24.
+//
+
+import Foundation
+
+let number = readLine()!
+
+let input = readLine()!
+var sum = 0
+
+for i in input {
+    sum += Int(String(i))!
+}
+
+print(sum)
+
+ 
