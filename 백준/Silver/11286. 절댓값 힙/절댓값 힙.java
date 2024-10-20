45918606f405ba446a57b2a44e105f69eaf1fc54
HongBeom <79892287+son7877@users.noreply.github.com>
2024-10-20 17:48:16 +0900

    [Silver I] Title: 절댓값 힙, Time: 468 ms, Memory: 27376 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,9 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PriorityQueue<Integer> pq= new PriorityQueue<>((o1, o2) -> { // 절대값 비교 및 음수 우선 정렬
             int abs1 = Math.abs(o1);
             int abs2 = Math.abs(o2);
@@ -14,10 +16,11 @@ public class Main {
                 return abs1 - abs2;
             }
         });
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
+        // 절댓값 힙 -> 절댓값이 작은 값이 먼저 나오도록
         for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
+            int x = Integer.parseInt(br.readLine());
             if (x == 0) {
                 if (pq.isEmpty()) {
                     System.out.println(0);
