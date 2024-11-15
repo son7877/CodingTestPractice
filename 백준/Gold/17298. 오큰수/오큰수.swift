2554f716abb5a775ace78f529eff51b93b0522e7
HongBeom <79892287+son7877@users.noreply.github.com>
2024-11-15 16:13:22 +0900

    [Gold IV] Title: 오큰수, Time: 584 ms, Memory: 143624 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.swift
@@ -0,0 +1,37 @@
+//
+//  main.swift
+//  BJ17298
+//
+//  Created by 안홍범 on 11/15/24.
+//
+
+import Foundation
+
+// 오큰수: 오른쪽에 있으면서 자신보다 큰 수 중 가장 왼쪽에 있는 수
+
+let N = Int(readLine()!)!
+
+let N_arr = readLine()!
+    .split(separator: " ")
+    .map{Int($0)!}
+
+func solution() {
+    var stack = [Int]()
+    var result = Array(repeating: -1, count: N)
+    
+    for i in 0..<N {
+        // 스택이 비어있지 않고, 현재 수가 스택의 가장 위의 수보다 클 때
+        while !stack.isEmpty && N_arr[stack.last!] < N_arr[i] {
+            result[stack.removeLast()] = N_arr[i]
+        }
+        stack.append(i)
+    }
+    
+    while !stack.isEmpty {
+        result[stack.removeLast()] = -1
+    }
+    
+    print(result.map{String($0)}.joined(separator: " "))
+}
+
+solution()
