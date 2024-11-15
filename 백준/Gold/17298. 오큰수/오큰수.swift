7236bf9a5206a1bf6c804d157b10b6a548337606
HongBeom <79892287+son7877@users.noreply.github.com>
2024-11-15 16:24:01 +0900

    [Gold IV] Title: 오큰수, Time: 576 ms, Memory: 143624 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.swift
+++ 백준/Gold/17298. 오큰수/오큰수.swift
@@ -22,7 +22,7 @@ func solution() {
     for i in 0..<N {
         // 스택이 비어있지 않고, 현재 수가 스택의 가장 위의 수보다 클 때
         while !stack.isEmpty && N_arr[stack.last!] < N_arr[i] {
-            result[stack.removeLast()] = N_arr[i]
+            result[stack.remove(at: stack.count-1)] = N_arr[i]
         }
         stack.append(i)
     }
