6c20c6a18d0fdbd16f9d402ab482923a57aedd36
HongBeom <79892287+son7877@users.noreply.github.com>
2024-12-03 19:54:25 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 216 ms, Memory: 80488 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.swift
@@ -0,0 +1,45 @@
+//
+//  main.swift
+//  BJ6588
+//
+//  Created by 안홍범 on 12/3/24.
+//
+
+import Foundation
+
+var prime = [Bool](repeating: true, count: 1000001)
+
+func isPrime() {
+    prime[0] = false
+    prime[1] = false
+    
+    for i in 2...1000000 {
+        if prime[i] {
+            for j in stride(from: i * 2, through: 1000000, by: i) { // i의 배수는 소수가 아니므로
+                prime[j] = false
+            }
+        }
+    }
+}
+
+func solution(_ input: Int) {
+    for i in 3...input {
+        if prime[i] && prime[input-i] { // 소수이면서 두 수의 합이 input인 경우
+            print("\(input) = \(i) + \(input-i)")
+            return
+        }
+    }
+    
+    print("Goldbach's conjecture is wrong.")
+}
+
+isPrime()
+
+while true {
+    let input = Int(readLine()!)
+    if input == 0 {
+        break
+    }
+    
+    solution(input!)
+}
