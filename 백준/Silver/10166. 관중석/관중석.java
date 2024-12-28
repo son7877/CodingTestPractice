58d7cc0f077c607ed9b3ecd95c8dbf623fdb0d9b
HongBeom <79892287+son7877@users.noreply.github.com>
2024-12-28 15:44:56 +0900

    [Silver I] Title: 관중석, Time: 228 ms, Memory: 18168 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.java
+++ 백준/Silver/10166. 관중석/관중석.java
@@ -1,33 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    private static final int D1 = sc.nextInt();
-    private static final int D2 = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
 
-    public static void main(String[] args) {
-        solution();
+        int D1 = Integer.parseInt(inputs[0]);
+        int D2 = Integer.parseInt(inputs[1]);
+
+        solution(D1, D2);
     }
 
-    static void solution() {
-        Set<String> uniqueAngles = new HashSet<>();
+    static void solution(int D1, int D2) {
+//        Set<Integer> uniqueAngles = new HashSet<>();
+        boolean visited[][] = new boolean[D2 + 1][D2 + 1];
 
-        for(int i=D1; i<=D2; i++) {
-            for(int j=1; j<=i; j++) {
+        for (int i = D1; i <= D2; i++) {
+            for (int j = 1; j <= i; j++) {
                 int gcd = gcd(i, j);
-                uniqueAngles.add((i/gcd) + "/" + (j/gcd));
+                int d1 = i / gcd;
+                int d2 = j / gcd;
+//                uniqueAngles.add(d1*1000000 + d2);
+                visited[d1][d2] = true;
             }
         }
 
-        System.out.println(uniqueAngles.size());
+//        System.out.println(uniqueAngles.size());
+        int cnt = 0;
+        for (int i = 1; i <= D2; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (visited[i][j]) {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
     }
 
-    static int gcd(int a, int b) {
+    static int gcd(int a, int b) { // 최대공약수
+        // 재귀
         if (b == 0) {
             return a;
+        } else {
+            return gcd(b, a % b);
         }
-        return gcd(b, a % b);
     }
 }
