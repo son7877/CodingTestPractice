a2b2912a7be3f8b5da76d38518d5619c5b99e745
HongBeom <79892287+son7877@users.noreply.github.com>
2025-01-21 16:24:45 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1536 ms, Memory: 187528 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.swift
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.swift
@@ -11,50 +11,58 @@ let input = readLine()!
     .split(separator: " ")
     .map { Int($0)! }
 
-let (N, M, K, X) = (input[0], input[1], input[2], input[3])
+let (N, M, K, X) = (input[0], input[1], input[2], input[3]) // N: 도시 개수, M: 도로 개수, K: 거리 정보, X: 출발 도시 번호
+var arr = [[Int]]()
+
+for _ in 0..<M {
+    let edge = readLine()!
+        .split(separator: " ")
+        .map { Int($0)! }
+    
+    arr.append(edge)
+}
 
 func solution() {
-    var graph = Array(repeating: [Int](), count: N + 1) // 단방향 연결 그래프
+    var graph = Array(repeating: [Int](), count: N + 1)
+    var visited = Array(repeating: false, count: N + 1) // 방문 여부
+    var distance = Array(repeating: -1, count: N + 1) // 거리 정보
     
-    for _ in 0..<M {
-        let link = readLine()!
-            .split(separator: " ")
-            .map { Int($0)! }
-        graph[link[0]].append(link[1]) // 단방향 연결
+    for i in 0..<M {
+        let (a, b) = (arr[i][0], arr[i][1])
+        graph[a].append(b) // 단방향
     }
     
-    var distances = Array(repeating: -1, count: N + 1) // 거리 배열 (-1로 초기화)
-    var result = [Int]() // 결과 배열
+    var queue = [X] // 시작 도시를 큐에 삽입
+    visited[X] = true
+    distance[X] = 0
+    var front = 0 // 큐의 가장 앞쪽을 가리키는 인덱스
     
-    func bfs(_ start: Int) {
-        var queue = [(start, 0)] // (노드, 거리) 형태로 저장
-        var front = 0
-        distances[start] = 0 // 시작점의 거리는 0
+    while front < queue.count {
+        let current = queue[front]
+        front += 1
         
-        while front < queue.count {
-            let (node, dist) = queue[front]
-            front += 1
+        for i in 0..<graph[current].count {
+            let next = graph[current][i] // 현재 도시와 연결된 도시
             
-            if dist == K {
-                result.append(node)
-            }
-            
-            for nextNode in graph[node] {
-                if distances[nextNode] == -1 {
-                    distances[nextNode] = dist + 1
-                    queue.append((nextNode, dist + 1))
-                }
+            if !visited[next] { // 방문하지 않은 도시라면
+                queue.append(next)
+                visited[next] = true // 방문 처리
+                distance[next] = distance[current] + 1 // 거리 정보 갱신
             }
         }
     }
     
-    bfs(X)
+    // 거리가 K인 도시가 존재하는지 확인
+    var check = false
+    for i in 1...N {
+        if distance[i] == K {
+            print(i)
+            check = true
+        }
+    }
     
-    if result.isEmpty {
+    if !check {
         print(-1)
-    } else {
-        result.sort() // 결과를 오름차순으로 정렬
-        result.forEach { print($0) }
     }
 }
 
