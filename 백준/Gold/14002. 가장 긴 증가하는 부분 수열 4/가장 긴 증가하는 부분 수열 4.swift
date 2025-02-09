4458cb04e09e17ac0290794084c40c2f79bed997
HongBeom <hongbeom7877@naver.com>
2025-02-09 20:02:30 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.swift
@@ -0,0 +1,72 @@
+//
+//  main.swift
+//  BJ14003
+//
+//  Created by 안홍범 on 2/6/25.
+//
+
+import Foundation
+
+let N = Int(readLine()!)!
+let arr = readLine()!
+    .split(separator: " ")
+    .map{Int($0)!}
+
+var dp = [Int]() // dp[i] : 길이가 i인 증가하는 부분 수열 중 마지막 수의 최솟값
+var indices = [Int]()// dp[i]의 인덱스
+var prev = [Int](repeating: -1, count: N) // 이전 인덱스
+
+func solution() {
+    for i in 0..<N {
+        let value = arr[i]
+        let pos = binarySearch(value)
+        
+        if pos < dp.count {
+            dp[pos] = value
+            indices[pos] = i
+        } else {
+            dp.append(value)
+            indices.append(i)
+        }
+        
+        if pos > 0 {
+            prev[i] = indices[pos-1]
+        }
+        
+    }
+    
+//    print(dp)
+//    print(indices)
+//    print(prev)
+    
+    var answer = [Int]()
+    var index = indices[indices.count-1]
+    
+    while index != -1 {
+        answer.append(arr[index])
+        index = prev[index]
+    }
+    answer.reverse()
+    
+    print(dp.count)
+    print(answer.map{String($0)}.joined(separator: " "))
+}
+
+func binarySearch(_ target: Int) -> Int {
+    var start = 0
+    var end = dp.count
+    
+    while start < end {
+        let mid = (start + end) / 2
+        
+        if dp[mid] < target {
+            start = mid + 1
+        } else {
+            end = mid
+        }
+    }
+    
+    return start
+}
+
+solution()
