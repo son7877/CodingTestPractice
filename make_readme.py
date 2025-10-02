6c007e294ab0e3aac06f3820cfc6457909647b4b
HongBeom <hongbeom7877@naver.com>
2025-10-02 16:14:57 +0900

    Add directory handling for specific categories

--- make_readme.py
+++ make_readme.py
@@ -35,6 +35,10 @@ def main():
     if directory not in directories:
       if directory in ["백준", "프로그래머스"]:
         content += "## 📚 {}\n".format(directory)
+      else:
+        content += "### 🚀 {}\n".format(directory)
+        content += "| 문제번호 | 링크 |\n"
+        content += "| ----- | ----- |\n"
       directories.append(directory)
       
     for file in files:
