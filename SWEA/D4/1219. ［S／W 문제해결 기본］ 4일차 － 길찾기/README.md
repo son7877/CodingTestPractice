4f997e10c051c6da4c21055baaa06f749f58e85a
HongBeom <hongbeom7877@naver.com>
2025-10-31 17:28:44 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 길찾기, Time: 182 ms, Memory: 34,944 KB -BaekjoonHub

---
+++ SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/README.md
@@ -0,0 +1,15 @@
+# [D4] [S/W 문제해결 기본] 4일차 - 길찾기 - 1219 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14geLqABQCFAYD) 
+
+### 성능 요약
+
+메모리: 34,944 KB, 시간: 182 ms, 코드길이: 1,786 Bytes
+
+### 제출 일자
+
+2025-10-31 17:28
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=10;
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            // 답안
+            int[] line = Arrays.stream(sc.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            int[] route = Arrays.stream(sc.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            int answer = getAnswer(route);
+
+            System.out.println("#" + line[0] + " " + answer);
+        }
+    }
+
+    private static int getAnswer(int [] route) {
+        int answer =0;
+
+        // 경로 map (일방통행)
+        HashMap<Integer,  List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < route.length/2; i++) {
+            int from = route[i*2];
+            int to = route[i*2+1];
+
+            if (!map.containsKey(from)) {
+                map.put(from, new ArrayList<>());
+            }
+            map.get(from).add(to);
+
+        }
+        
+        Set<Integer> visited = new HashSet<>();
+        return dfs(0, 99, map, visited) ? 1 : 0;
+    }
+    
+    private static boolean dfs(
+            int start, int end, HashMap<Integer, List<Integer>> map, Set<Integer> visited) {
+        if (start == end) {
+            return true;
+        }
+
+        visited.add(start);
+
+        if (!map.containsKey(start)) {
+            return false;
+        }
+
+        for (int next : map.get(start)) {
+            if (!visited.contains(next)) { // 
+                if (dfs(next, end, map, visited)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
