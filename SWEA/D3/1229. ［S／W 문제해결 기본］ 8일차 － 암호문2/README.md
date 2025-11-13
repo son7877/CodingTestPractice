bd03092bd17d8758babf2e73c61400dca846fbe9
HongBeom <hongbeom7877@naver.com>
2025-11-13 16:51:24 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 193 ms, Memory: 34,816 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/README.md
@@ -0,0 +1,15 @@
+# [D3] [S/W 문제해결 기본] 8일차 - 암호문2 - 1229 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14yIsqAHYCFAYD) 
+
+### 성능 요약
+
+메모리: 34,816 KB, 시간: 193 ms, 코드길이: 1,736 Bytes
+
+### 제출 일자
+
+2025-11-13 16:51
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = 10;
+
+        for(int test_case = 1; test_case <=10; test_case++){
+            int N = sc.nextInt();
+            sc.nextLine(); 
+            int[] secret = Arrays.stream(sc.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            int M = sc.nextInt();
+            sc.nextLine(); 
+            String[] input = sc.nextLine().trim().split(" ");
+
+            ArrayList<Integer> arr = new ArrayList<>();
+
+            for(int number: secret) {
+                arr.add(number);
+            }
+
+            int idx = 0;
+            while(idx < input.length) {
+                String command = input[idx];
+
+                if(command.equals("I")) {
+                    int x = Integer.parseInt(input[idx + 1]);
+                    int y = Integer.parseInt(input[idx + 2]);
+                    for(int i = 0; i < y; i++) {
+                        int s = Integer.parseInt(input[idx + 3 + i]);
+                        arr.add(x + i, s);
+                    }
+                    idx += 3 + y;
+                } else if(command.equals("D")) {
+                    int x = Integer.parseInt(input[idx + 1]);
+                    int y = Integer.parseInt(input[idx + 2]);
+                    for(int i = 0; i < y; i++) {
+                        arr.remove(x);
+                    }
+                    idx += 3;
+                }
+            }
+
+            System.out.print("#"+test_case+" ");
+            for(int i=0;i<10;i++){
+                System.out.print(arr.get(i)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
